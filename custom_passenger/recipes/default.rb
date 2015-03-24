f11800ca3974a5ecfe12e30e5bff7bb03c202ab3
Deepak Kumar <deepakkashyap01@hotmail.com>
2015-03-25 02:32:40 +0530

    Update default.rb

--- custom_passenger/recipes/default.rb
+++ custom_passenger/recipes/default.rb
@@ -2,5 +2,5 @@
 include_recipe "nginx_passenger"
 include_recipe "deploy"
 
-default.nginx_passenger.sites_dir="#{node[:deploy][application][:deploy_to]}/current"
+node.default.nginx_passenger.sites_dir="#{node[:deploy][application][:deploy_to]}/current"
 
