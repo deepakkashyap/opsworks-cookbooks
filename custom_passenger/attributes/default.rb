1de64f03ef351e2b5548ec4c353c96bd57f90213
Deepak Kumar <deepakkashyap01@hotmail.com>
2015-03-25 12:32:58 +0530

    Update default.rb

--- custom_passenger/attributes/default.rb
+++ custom_passenger/attributes/default.rb
@@ -1,3 +1,6 @@
+
+include_attribute "deploy::deploy"
+
 default["custompassenger"]["clonelocation"] = "/home/ubuntu/test"
 default["custompassenger"]["repourl"] = "https://github.com/RailsApps/rails-signup-download.git"
 default["custompassenger"]["rubyversion"]= "1.9.1"
