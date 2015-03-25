c9e83c3fc99fa3e04f0d6c844358f057f262e39d
Deepak Kumar <deepakkashyap01@hotmail.com>
2015-03-25 13:24:39 +0530

    Update default.rb

--- custom_passenger/recipes/default.rb
+++ custom_passenger/recipes/default.rb
@@ -3,7 +3,7 @@ include_recipe "nginx_passenger"
 
 nginx_passenger_site "test" do
   action :create
-  dir    "#{node[:opsworks][:applications][0][:name]}/current"
+  dir    "/srv/www/test-app/current"
   server "#{node[:opsworks][:instance][:ip]}"
   rails_env "production"
 end
