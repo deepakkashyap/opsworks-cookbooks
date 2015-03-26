19065ecca2e31eb29b2663ff3cda6401994b61b5
Deepak Kumar <deepakkashyap01@hotmail.com>
2015-03-26 18:22:28 +0530

    Update rollback.rb

--- deploy/recipes/rollback.rb
+++ deploy/recipes/rollback.rb
@@ -1,19 +1,22 @@
 
 
 if app[:state] == 'restart'
+    Chef::Log.warn("Restart gonna work")
+    
   directory "/var/lib/restart" do
   owner 'root'
   group 'root'
   mode '0755'
   action :create
   end
-end
-
-elsif app[:state] == 'stop'
+elseif app[:state] == 'stop'
+    Chef::Log.warn("Restart gonna work")
+    
   directory "/var/lib/stop" do
   owner 'root'
   group 'root'
   mode '0755'
   action :create
   end
+end
 end
