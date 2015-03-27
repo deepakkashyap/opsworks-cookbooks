7f41166a53a54f4f3addc17d6b6cb1046210f99a
Deepak Kumar <deepakkashyap01@hotmail.com>
2015-03-27 12:41:43 +0530

    Update rollback.rb

--- deploy/recipes/rollback.rb
+++ deploy/recipes/rollback.rb
@@ -1,17 +1,47 @@
 
+if "#{node[:app][:state]}" == 'deploy' 
+    include_recipe "deploy"
 
-if "#{node[:app][:state]}" == 'restart' 
+     apt_package "libmcrypt-dev" do
+         action :install
+     end
+
+     node[:deploy].each do |application, deploy|
+
+       Chef::Log.info("Deploying application #{application} on #{node[:opsworks][:instance][:hostname]}")
+
+       if deploy[:application_type] != 'other'
+          Chef::Log.warn("Skipping deploy::web application #{application}")
+          next
+       end
+
+       opsworks_deploy_dir do
+           user deploy[:user]
+            group deploy[:group]
+            path deploy[:deploy_to]
+        end
+
+       opsworks_deploy do
+          deploy_data deploy
+          app application
+       end
+     end
+  
+  include_recipe "nginx_passenger"
+  nginx_passenger_site "test" do
+     action :create
+     dir    "/srv/www/sample_app/current"
+     server "#{node[:opsworks][:instance][:ip]}"
+     rails_env "production"
+  end
+
+elsif "#{node[:app][:state]}" == 'restart' 
   Chef::Log.info("#####   Restarting nginx   #####")
     
   service "nginx" do
 
    action :restart
    end
- else
-  directory "/var/lib/deepak" do
-  owner 'root'
-  group 'root'
-  mode '0755'
-  action :create
- end
+ 
+  
 end
